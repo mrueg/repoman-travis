75110f2fbd64a8677792e722d256a075b2b36d5f
William L. Thomson Jr <wlt@o-sinc.com>
2016-12-06 20:35:51 -0500

    spinner.sh: Quotes SC2086, Removed $ SC2004 $() SC2145, Swap @ -> * SC2145

--- spinner.sh
+++ spinner.sh
@@ -6,16 +6,16 @@ spinner() {
 	local PID=$1
 	local SPIN="/-\|"
 	local i=0
-	while kill -0 $PID >/dev/null 2>&1; do
+	while kill -0 "$PID" >/dev/null 2>&1; do
 		printf "\r%s" "${SPIN:$i:1}"
-		i=$(($i+1))
+		i=$((i+1))
 		[[ $i -eq 4 ]] && i=0
 		sleep 0.5;
 	done
 }
 
 cat /tmp/update 2>/dev/null
-$(script -q -e /dev/null -c "${@} 2>&1" >/tmp/output) &
+script -q -e /dev/null -c "${*} 2>&1" >/tmp/output &
 PID=$!
 spinner "${PID}" &
 wait ${PID}
