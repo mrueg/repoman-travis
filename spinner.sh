d5b843eb11d6831f1b27f9f21c8968be41807db1
Manuel Rüger <manuel@rueg.eu>
2016-12-12 23:16:07 +0100

    spinner.sh: Update

--- spinner.sh
+++ spinner.sh
@@ -15,7 +15,7 @@ spinner() {
 }
 
 cat /tmp/update 2>/dev/null
-script -q -e /dev/null -c "${*} 2>&1" >/tmp/output &
+script -q -e /dev/null -c "${@}" >/tmp/output 2>&1 &
 PID=$!
 spinner "${PID}" &
 wait ${PID}
